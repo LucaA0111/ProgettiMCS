630e9fe2d66838764ce396d77f947ff15424d7a3
LucaA0111 <luca,acquati10@gmail.com>
2025-06-21 17:18:40 +0200

    Renamed images directory

--- Progetto-2/Seconda Parte/utils/utils.py
+++ Progetto-2/Seconda Parte/utils/utils.py
@@ -3,7 +3,7 @@ import os
 class Utils:
 
     PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    IMAGE_DIR = os.path.join(PROJECT_ROOT, 'immmagini')
+    IMAGE_DIR = os.path.join(PROJECT_ROOT, 'images')
     OUTPUT_DIR = os.path.join(PROJECT_ROOT, 'compressed_images')
 
     DEFAULT_F = 8
